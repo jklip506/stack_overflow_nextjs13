9b27a4b6cec21e43d5f05f009503f51f89158fc6
jklip506 <96444138+jklip506@users.noreply.github.com>
2024-01-03 21:07:41 -0700

    finsihed remainder of answers form ui

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,6 +21,7 @@ import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
 import { useRouter, usePathname } from "next/navigation";
+import { useTheme } from "@/context/ThemeProvider";
 
 const type: any = "create";
 
@@ -33,6 +34,7 @@ const Question = ({ mongoUserId }: Props) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
   const pathname = usePathname();
+  const { mode } = useTheme();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
@@ -158,6 +160,8 @@ const Question = ({ mongoUserId }: Props) => {
                         "alignright alignjustify | bullist numlist ",
                       content_style:
                         "body { font-family:Inter; font-size:16px }",
+                      skin: mode === "dark" ? "oxide-dark" : "oxide",
+                      content_css: mode === "dark" ? "dark" : "light",
                     }}
                   />
                 </FormControl>
